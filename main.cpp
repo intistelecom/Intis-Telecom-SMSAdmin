66677d323b0a693c05366e232e24ebdd7c6fa508
Alexander M Artamonov <art.alex.m@gmail.com>
2014-01-21 13:03:24 +0400

    обавил линковку libxml++

--- main.cpp
+++ main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <libxml++/libxml++.h>
 
 using namespace std;
+using namespace Glib;
+using namespace xmlpp;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    // Parse the file
+    DomParser parser;
+    parser.parse_file("file.xml");
+    Node* rootNode = parser.get_document()->get_root_node();
+
+    // Xpath query
+    NodeSet result = rootNode->find("/root/a/b/@attr");
+
+    // Get first node from result
+    Node *firstNodeInResult = result.at(0);
+    // Cast to Attribute node (dynamic_cast on reference can throw [fail fast])
+    Attribute &attribute = dynamic_cast<Attribute&>(*firstNodeInResult);
+
+    // Get value of the attribute
+    ustring attributeValue = attribute.get_value();
+
+    // Print attribute value
+    cout << attributeValue << endl;
     return 0;
 }
 
