9490db5df823a8c0d394a0e2e9263a21e6e0263c
Alexander M Artamonov <art.alex.m@gmail.com>
2014-02-20 13:35:35 +0400

    SA #12: Add readme and license

--- src/smsadmin.cpp
+++ src/smsadmin.cpp
@@ -11,7 +11,7 @@ string help()
 {
     ostringstream help;
     help << endl
-         << "Smsadmin version: " << smsadmin::VERSION << " \"Could Start\"" << endl
+         << "Smsadmin version: " << SMSADMIN_VERSION << " \"" << SMSADMIN_VER_NAME << "\"" << endl
          << "Sms16.ru API version: " << sms16xapi::API_VERSION << endl
          << "Build date: " << SMSADMIN_BUILD_DATE << endl << endl
          << config::Config::get_instance().help()
