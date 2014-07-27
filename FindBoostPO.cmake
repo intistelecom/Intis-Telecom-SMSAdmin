ce090906575111ebab23e952e30bf67e9c2fb690
Alexander M Artamonov <art.alex.m@gmail.com>
2014-07-27 14:07:05 +0400

    SA #23: CMake boost finder

--- FindBoostPO.cmake
+++ FindBoostPO.cmake
@@ -1,6 +1,10 @@
 # Try to find libboost_program_options
-set(Boost_MIN_VERSION 1.41)
+set(Boost_MIN_VERSION 1.53)
 
 find_package(Boost ${Boost_MIN_VERSION} REQUIRED COMPONENTS program_options)
 include_directories(${Boost_INCLUDE_DIRS})
 target_link_libraries(${TARGET} ${Boost_LIBRARIES})
+
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+    message(STATUS "-L${Boost_LIBRARIES} -I${Boost_INCLUDE_DIRS}")
+endif()
--- FindLibXML++.cmake
+++ FindLibXML++.cmake
@@ -8,13 +8,6 @@
 # LIBXMLPP_LINKFLAGS
 # LIBXMLPP_MIN_VERSION
 
-#set(LIBXMLPP_MIN_VERSION 2.35)
-#include(FindPkgConfig)
-#pkg_check_modules(LIBXMLPP libxml++-2.6>=${LIBXMLPP_MIN_VERSION})
-#message(STATUS "Found LibXml++: ${LIBXMLPP_LIBRARIES}")
-#include_directories(BEFORE ${LIBXMLPP_INCLUDE_DIRS})
-#target_link_libraries(smsadmin ${LIBXMLPP_LIBRARIES})
-
 set(LIBXMLPP_MIN_VERSION 2.6)
 set(LIBXMLPP_FOUND FALSE)
 
@@ -33,7 +26,7 @@ if (LIBXMLPP_INCLUDE_DIRS AND LIBXMLPP_LIBRARIES)
 endif (LIBXMLPP_INCLUDE_DIRS AND LIBXMLPP_LIBRARIES)
 
 if (LIBXMLPP_FOUND)
-    message(STATUS "Found tinyxml: headers ${LIBXMLPP_INCLUDE_DIRS}, libs ${LIBXMLPP_LIBRARIES}")
+    message(STATUS "Found tinyxml: -I${LIBXMLPP_INCLUDE_DIRS} -L${LIBXMLPP_LIBRARIES}")
     include_directories(${LIBXMLPP_INCLUDE_DIRS})
     target_link_libraries(${SMS16XAPI} ${LIBXMLPP_LIBRARIES})
 else (LIBXMLPP_FOUND)
