2ff00ace1bf01b8ac39edd0e380727aa6ff0ef16
Alexander M Artamonov <art.alex.m@gmail.com>
2015-06-28 20:29:10 +0300

    Add CentOS6.5 toolchain for crosscompiling

--- FindCurl.cmake
+++ FindCurl.cmake
@@ -1,24 +1,23 @@
 # locate CURL library
 
-if(UNIX)
+if(UNIX AND NOT CMAKE_CROSSCOMPILING)
     set(CURL_MIN_VERSION 7.19)
     include(FindPkgConfig)
     pkg_check_modules(CURL libcurl>=${CURL_MIN_VERSION} REQUIRED)
     include_directories(BEFORE ${CURL_INCLUDE_DIRS})
     target_link_libraries(${TARGET} ${CURL_LIBRARIES})
-endif(UNIX)
+endif()
 
-if(WIN32)
+if(WIN32 OR CMAKE_CROSSCOMPILING)
     find_path(LIBCURL_INCLUDE_DIRS
       NAMES curl.h
       PATHS "include;/usr/include;/usr/local/include"
+      PATH_SUFFIXES curl
     )
-
-    set(LIBCURL_INCLUDE_DIRS /opt/mingw-w32/include)
 
     find_library(LIBCURL_LIBRARIES
       NAMES curl
-      PATHS ".;c:/windows/system32;/usr/lib;/usr/local/lib"
+      PATHS ".;c:/windows/system32;lib;/usr/lib;/usr/local/lib;/usr/lib64;/usr/local/lib64"
     )
 
     if (LIBCURL_INCLUDE_DIRS AND LIBCURL_LIBRARIES)
@@ -32,4 +31,4 @@ if(WIN32)
     else (LIBCURL_FOUND)
         message(FATAL_ERROR "Required library curl not found")
     endif (LIBCURL_FOUND)
-endif(WIN32)
+endif()
