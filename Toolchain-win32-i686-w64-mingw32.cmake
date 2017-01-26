86078cbece05e36d4bea1494c94f1ff1830aef40
Alexander M. Artamonov <art.alex.m@gmail.com>
2017-01-26 11:46:16 +0300

    HF: Fix Win32 toolchain

--- Toolchain-win32-i686-w64-mingw32.cmake
+++ Toolchain-win32-i686-w64-mingw32.cmake
@@ -1,13 +1,13 @@
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
 
-# which compilers to use for C and C++
-SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
-SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
-SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
-
 # here is the target environment located
 SET(CMAKE_FIND_ROOT_PATH /opt/mingw-w32)
+
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER ${CMAKE_FIND_ROOT_PATH}/bin/i686-w64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER ${CMAKE_FIND_ROOT_PATH}/bin/i686-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER ${CMAKE_FIND_ROOT_PATH}/bin/i686-w64-mingw32-windres)
 
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search 
